a42d008c194478523954737390a34a021c48c06b
FabioZumbi12 <zumbi12doze@gmail.com>
2018-04-20 03:50:32 -0300

    Move files

---
+++ PropertiesLoader/Main.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace PropertiesLoader
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.fileLocCustom = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileLocCustom
+            // 
+            this.fileLocCustom.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.fileLocCustom.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.fileLocCustom.Location = new System.Drawing.Point(12, 44);
+            this.fileLocCustom.Name = "fileLocCustom";
+            this.fileLocCustom.ReadOnly = true;
+            this.fileLocCustom.Size = new System.Drawing.Size(460, 20);
+            this.fileLocCustom.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(712, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose the DEFAULT language first (en_US), then select your CUSTOM language to co" +
+    "py untraslated lines to your lang file.";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Enabled = false;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 110);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(893, 368);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(597, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Load CUSTOM";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(494, 40);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(97, 23);
+            this.buttonOpen.TabIndex = 2;
+            this.buttonOpen.Text = "Load EN_US";
+            this.toolTip1.SetToolTip(this.buttonOpen, "Load the file EN_US before load your custom file");
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Enabled = false;
+            this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSave.Location = new System.Drawing.Point(764, 40);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(140, 23);
+            this.buttonSave.TabIndex = 4;
+            this.buttonSave.Text = "Save to File";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.panel1.Location = new System.Drawing.Point(355, 253);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(236, 87);
+            this.panel1.TabIndex = 19;
+            this.panel1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(220, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mixing strings...";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(11, 484);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(729, 55);
+            this.textBox1.TabIndex = 20;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(746, 487);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 23);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Try To Translate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(746, 516);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 23);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Apply";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(858, 489);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(46, 20);
+            this.textBox2.TabIndex = 23;
+            this.textBox2.Text = "en";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(19, 47);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(201, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 544);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "No errors";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(826, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "by FabioZumbi12";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Search:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(11, 84);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(217, 20);
+            this.textBox3.TabIndex = 27;
+            this.toolTip1.SetToolTip(this.textBox3, "Enter to search");
+            this.textBox3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox3_KeyDown);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(255, 84);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(217, 20);
+            this.textBox4.TabIndex = 29;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(257, 67);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "Replace By:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(494, 81);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 30;
+            this.button4.Text = "Replace";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label7.Location = new System.Drawing.Point(764, 67);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 13);
+            this.label7.TabIndex = 31;
+            this.label7.Text = "File Saved!";
+            this.label7.Visible = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(917, 566);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fileLocCustom);
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "Properties/Lang Translator";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fileLocCustom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ PropertiesLoader/Main.cs
@@ -0,0 +1,239 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace PropertiesLoader
+{
+    public partial class Main : Form
+    {
+        string[] lines;
+        DataTable table;
+
+        public Main()
+        {
+            InitializeComponent();
+            
+            //progressbar
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.DoWork += new DoWorkEventHandler(backgroundWorker1_DoWork);
+            backgroundWorker1.ProgressChanged += new ProgressChangedEventHandler(backgroundWorker1_ProgressChanged);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Lang files (*.lang)|*.lang";
+            dialog.RestoreDirectory = true;
+            dialog.Title = "Choose DEFAULT lang file";
+            dialog.FileName = "en_US.lang";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                //store original lines
+                lines = File.ReadAllLines(dialog.FileName);
+
+                InitTable();
+
+                buttonSave.Enabled = false;
+
+                int line = 0;
+                foreach (string row in lines)
+                {
+                    line++;
+                    if (row.Split('=').Length == 2)
+                    {
+                        string key = row.Split('=')[0];
+                        if (key.Contains("*")) continue;
+                        table.Rows.Add(line, key, string.Join("=", row.Split('=').Skip(1).ToArray()));
+                    }                        
+                }
+            }            
+        }
+
+        private void InitTable()
+        {
+            table = new DataTable();
+            dataGridView1.DataSource = table;
+
+            table.Columns.Add("Line", typeof(int));
+            table.Columns.Add("Key", typeof(string));
+            table.Columns.Add("Translation", typeof(string));
+
+            dataGridView1.Columns["Line"].Width = 50;
+            dataGridView1.Columns["Key"].Width = 300;
+            dataGridView1.Columns["Key"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.Columns["Translation"].Width = 300;
+            dataGridView1.Columns["Translation"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Lang files (*.lang)|*.lang";
+            dialog.RestoreDirectory = true;
+            dialog.Title = "Choose YOUR CUSTOM lang file";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                fileLocCustom.Text = dialog.FileName;
+                
+                if (table == null || table.Rows.Count == 0)
+                {
+                    //load file
+                    lines = File.ReadAllLines(dialog.FileName);
+
+                    InitTable();
+
+                    int line = 0;
+                    foreach (string row in lines)
+                    {
+                        line++;
+                        if (row.Split('=').Length == 2)
+                        {
+                            string key = row.Split('=')[0];
+                            if (key.Contains("*")) continue;
+                            table.Rows.Add(line, key, string.Join("=", row.Split('=').Skip(1).ToArray()));
+                        }
+                    }
+                    EndLoading();
+                }
+                else
+                {
+                    button1.Enabled = false;
+                    buttonOpen.Enabled = false;
+                    buttonSave.Enabled = false;
+                    panel1.Visible = true;
+                    label7.Visible = false;
+
+                    backgroundWorker1.RunWorkerAsync();
+                }                
+            }
+        }
+
+        void EndLoading()
+        {
+            panel1.Visible = false;
+            dataGridView1.Enabled = true;
+            button1.Enabled = true;
+            buttonOpen.Enabled = true;
+            buttonSave.Enabled = true;
+            buttonSave.Enabled = true;
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            textBox1.Text = dataGridView1.SelectedRows[0].Cells["Translation"].Value.ToString();
+        }
+
+        void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int count = 0;
+            string[] custom = File.ReadAllLines(fileLocCustom.Text);
+
+            foreach (string row in custom)
+            {
+                count++;
+                if (row.Split('=').Length == 2)
+                {
+                    string key = row.Split('=')[0];
+                    string value = string.Join("=", row.Split('=').Skip(1).ToArray());
+
+                    if (key.Contains("*")) continue;
+
+                    DataRow dr = table.Select("Key = '" + key.Replace("'", "''") + "'").FirstOrDefault();
+                    if (dr != null && !dr["Translation"].Equals(value))
+                    {
+                        dr["Translation"] = value;
+                    }
+                }
+                double med = (double)count / custom.Length;
+                backgroundWorker1.ReportProgress((int)(med * 100));
+            }
+        }
+
+        void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            if (e.ProgressPercentage == 100)
+                EndLoading();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            foreach (DataRow row in table.Rows)
+            {
+                int line = int.Parse(row["Line"].ToString());
+                string key = row["Key"].ToString();
+                string value = row["Translation"].ToString();
+
+                lines[line-1] = key + "=" + value;
+            }
+            File.WriteAllLines(fileLocCustom.Text, lines);
+            label7.Visible = true;
+        }
+
+        private void textBox3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    object value = row.Cells["Translation"].Value;
+                    if (value != null && value.ToString().Contains(textBox3.Text))
+                    {
+                        row.Selected = true;
+                        dataGridView1.FirstDisplayedScrollingRowIndex = row.Index;
+                        break;
+                    }
+                }
+            }
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string strLangPair = string.Format("en|{0} ", textBox2);
+
+                // generate google translate request
+                // http://translate.google.com/translate_a/t?client=j&sl=en&tl=ru&text=some%20text
+                string url = String.Format("http://translate.google.com/translate_a/t?client=j&sl=en&tl={1}&text={0}",
+                  textBox1.Text, textBox2);
+
+                WebClient webClient = new WebClient();
+                byte[] resultBin = webClient.DownloadData(url);
+                string charset = webClient.ResponseHeaders["Content-Type"];
+                charset = charset.Substring(charset.LastIndexOf('=') + 1);
+
+                Encoding transmute = Encoding.GetEncoding(charset);
+                string result = transmute.GetString(resultBin);
+
+                // find result box and get a text
+                JObject item = JObject.Parse(result);
+                JToken token = item["sentences"];
+                String translation = string.Empty;
+
+                foreach (JToken childToken in token)
+                {
+                    string phrase = childToken["trans"].ToString();
+                    translation += phrase.TrimStart('"').TrimEnd('"');
+                }
+
+                textBox1.Text = translation;
+            }
+            catch (Exception ex)
+            {
+                label3.ForeColor = System.Drawing.Color.Red;
+                label3.Text = ex.Message;
+            }
+        }
+
+    }
+}
